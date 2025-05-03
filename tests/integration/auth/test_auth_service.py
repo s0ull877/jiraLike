93b427dc9a32b1114c51bb1d1d42089b68412119
Radmir <galiullinradmir17@gmail.com>
2025-05-03 11:40:37 +0300

    integration tests auth

--- tests/integration/auth/test_auth_service.py
+++ tests/integration/auth/test_auth_service.py
@@ -1,12 +1,13 @@
 
 import pytest
-from sqlalchemy import select, insert
+from faker import Faker
 
 from src.core.entities.auth import User
 from src.core.services.auth import AuthService
 
 pytestmark = pytest.mark.asyncio
 
+fake = Faker()
 
 async def test_temp(auth_service):
 
@@ -14,10 +15,53 @@ async def test_temp(auth_service):
     assert isinstance(auth_service, AuthService)
 
     user = User(
-        email="somsadsemail@mail.com",
-        password="somepassword"
+        email=fake.email(),
+        password=fake.name()
     )
     db_user = await auth_service.create_user(user)
 
-    print(f'\nUSER: {db_user}\n')
     assert db_user != None
+
+
+async def test_login__failed(auth_service):
+
+    email = fake.email()
+    password = fake.name()
+
+    user = User(
+        email=email,
+        password=password
+    )
+    db_user = await auth_service.create_user(user)
+    
+    assert db_user != None
+
+    with pytest.raises(Exception) as exc_info:
+        token = await auth_service.login(email, password)
+    
+    assert exc_info.value.args[0] == 'User email is not verified! Check email!'
+
+
+
+async def test_login__success(auth_service):
+
+    email = fake.email()
+    password = fake.name()
+
+    user = User(
+        email=email,
+        password=password
+    )
+    db_user = await auth_service.create_user(user)
+    verification = await auth_service.create_verify_code(db_user)
+    
+    assert db_user != None
+    assert verification != None
+
+    activated_user = await auth_service.activate_user(code=verification.code, user=db_user)
+
+    assert activated_user.is_active == True
+
+    token = await auth_service.login(email, password)
+    
+    assert token != None
